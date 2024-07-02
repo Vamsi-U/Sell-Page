f3f09aa0dfbee4c4efb6031b064559d9876dacbc
UNTA VAMSI <untavamsi@UNTAs-Air.ca.iitmlan>
2024-07-02 17:17:11 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BicycleForm from './Page/BicycleSell/BicycleForm';
+
+function App() {
+  return (
+    <div className="container mx-auto py-8">
+      <BicycleForm />
+    </div>
+  );
+}
+
+export default App;
